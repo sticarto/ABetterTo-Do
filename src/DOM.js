059c56be00ae6642681496b830cd52a296ee1780
Sticarto <123905512+sticarto@users.noreply.github.com>
2026-02-11 14:22:24 -0800

    Added type checking to the displayTask function

--- src/DOM.js
+++ src/DOM.js
@@ -1,11 +1,18 @@
 // Handles the UI
 
+import { ToDoItem } from "./todo";
+
 const DOM = (function () {
     const sidebar = document.querySelector(".sidebar");
     const mainpage = document.querySelector(".main-page");
 
-    // Display to do object's information
+    // Display to-do object's information
     const displayTask = (task) => {
+        if (!(task instanceof ToDoItem)) {
+            console.error("Error: the parameter must be an instance of the ToDoItem class");
+            return;
+        }
+
         const item = document.createElement('p');
         item.textContent = task.info;
         mainpage.appendChild(item);
