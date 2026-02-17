73c73c67adf4f5f421b9f0f36574a632a8535bb9
Sticarto <123905512+sticarto@users.noreply.github.com>
2026-02-17 14:22:27 -0800

    Allow project buttons to update the main page display

--- src/DOM.js
+++ src/DOM.js
@@ -7,7 +7,7 @@ const DOM = (function () {
     const mainpage = document.querySelector(".main-page");
 
     // Display to-do object's information
-    const displayTask = (task) => {
+    const displayToDo = (task) => {
         if (!(task instanceof ToDoItem)) {
             console.error("Error: the parameter must be an instance of the ToDoItem class");
             return;
@@ -18,20 +18,39 @@ const DOM = (function () {
         mainpage.appendChild(item);
     }
     
-    
+
     const addProjectToSidebar = (project) => {
         const projectTitle = document.createElement('button');
 
         projectTitle.textContent = project.title;
+
+        projectTitle.addEventListener('click', () => displayProject(project));
 
         sidebar.appendChild(projectTitle);
 
     }
 
     // something to display the project title inside the main page
+    const displayProject = (project) => {
+
+        while (mainpage.firstChild) {
+            mainpage.removeChild(mainpage.firstChild);
+        }
+        
+        const projectTitle = document.createElement('h1');
+        projectTitle.textContent = project.title;
+        mainpage.appendChild(projectTitle);
+
+        // iterate through the to do lists the project has stored and display them in order
+        project.lists.forEach(todo => {
+            displayToDo(todo);
+        });
 
 
-    return {displayTask, addProjectToSidebar}
+    }
+
+
+    return {displayProject, addProjectToSidebar}
 
 })();
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { DOM } from "./DOM";
 import { Project } from "./project";
 
 const defaultToDoItem = new ToDoItem('Take out the trash', "Write something here", "due date", "high");
+const secondToDoItem = new ToDoItem('Walk the dog', "Write something here", "due date", "medium");
 const defaultProject = new Project('First Project');
 const secondProject = new Project('Second Project');
 
 defaultProject.addToDoList(defaultToDoItem);
+defaultProject.addToDoList(secondToDoItem);
 console.log(defaultProject.lists)
 
-DOM.displayTask(defaultToDoItem);
+DOM.displayProject(defaultProject);
 DOM.addProjectToSidebar(defaultProject);
 DOM.addProjectToSidebar(secondProject);
